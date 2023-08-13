77519349a2dd0c17fa9d4265ad0b8d3432d372b0
Raghav Rudhra <raghavrudhra28@gmail.com>
2023-08-13 12:42:16 -0400

    Adding broken eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { GradientBackgroundCon } from "@/components/QuoteGenerator";
+
 export default function Home({}) {
   return (
     <main>
-      <div><h1>HELLO WORLD</h1></div>
+      <div>
+        <h1>HELLO WORLD</h1>
+      <GradientBackgroundCon>
+        </GradientBackgroundCon>
+        </div>
     </main>
   )
 }
